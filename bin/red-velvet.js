8f99b5335cfc1a23a683084a01e1075d491d2831
Stefani Taylor <stefanitaylor@lc-stefanietaylor.local>
2017-07-17 19:39:33 -0700

    updating the CLI

--- bin/red-velvet.js
+++ bin/red-velvet.js
@@ -1,18 +1,22 @@
 #! /usr/bin/env node
-//this was taken from https://github.com/jaschaephraim/static-generator/blob/master/bin/stat-gen.js
 
-// Get command, defaulting to 'create'
-var cmd = process.argv[ 2 ] || 'create';
 // promise library
-var promise = require('bluebird');
+var promise = require('bluebird'),
+    colors      = require('colors'),
+    program = require('commander');
 
-// Execute command, passing any other arguments
-//require( '../commands/' + cmd )( process.argv.slice( 3 ) );
+program
+  .version('1.0.0')
+  .option('-c, --create [type]', 'Create the a new feature named [INSERT_NAME]', 'INSERT_NAME')
+  .parse(process.argv);
+
 
 var _create = require('../lib/create'),
     _walk = require('../lib/walk'),
-    feature_name = process.argv.slice(3);
+    feature_name = program.create;
 
+
+console.log(program.create)
 /**
  * Error handler
  */
@@ -20,9 +24,8 @@ function stderr (err) {
     console.log('Error: '.red + err.red);
     process.exit(1);
 }
-
-
-if(cmd === 'create'){
+//if create was passed then create a new feature
+if(program.create){
   _create(feature_name)
     .then(function(result){
       console.log(result)
